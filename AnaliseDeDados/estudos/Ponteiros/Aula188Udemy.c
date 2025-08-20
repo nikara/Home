c8cb453f9489fb69836337868312a183c8731bee
NikolasLamin <62860064+nikara@users.noreply.github.com>
2025-08-20 15:46:06 -0300

    Add pointer examples to Aula188Udemy.c
    Introduced pointer variables for 'idade' and 'sexo', and added print statements to display pointer addresses, contents, and dereferenced values. This enhances the demonstration of pointer usage in C for educational purposes.

--- AnaliseDeDados/estudos/Ponteiros/Aula188Udemy.c
+++ AnaliseDeDados/estudos/Ponteiros/Aula188Udemy.c
@@ -6,7 +6,15 @@ int main(){ // O que é e como criar um ponteiro na linguagem C ?
 int idade = 30;
 char sexo = 'f';
 
-printf("\n\t Endereco de idade: %p\tidade: %d\n", &idade, idade);
+int *pi = &idade;
+char *ps = &sexo;
+
+printf("\n\t %d\n", sizeof sexo);
+
+printf("\n\tEndereco de idade: %p\tidade: %d\n", &idade, idade);
 printf("\tEndereco de sexo: %p\tSexo: %c\n", &sexo,sexo);
+
+printf("\n\tEndereco de pi: %p\tConteudo de pi: %p\t Cont.apontado por pi: %d\n", &pi, pi, *pi);
+printf("\n\tEndereco de ps: %p\tConteudo de ps: %p\t Cont.apontadp por ps: %c\n",&ps, ps,*ps);
 	
 }
